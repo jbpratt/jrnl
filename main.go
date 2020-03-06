cda3e018b37a37a355ad79ff6ad32e948ffafb5d
jbpratt78 <jbpratt78@gmail.com>
2020-03-05 22:28:31 -0600

    remove windows test, test for editorNotSet

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"time"
 )
 
+var editorNotSet = errors.New("EDITOR env variable not set")
+
 func main() {
 	// Open a file named the current date. Insert the current time at the last line
+	// handle inputting the time with other editors. This seems to not work with vi
 	if err := run(
 		fmt.Sprintf("%s.md", time.Now().Format("2006-01-02")),
 		"-c", ":$put _",
@@ -21,7 +25,7 @@ func main() {
 func run(cmds ...string) error {
 	editor := os.Getenv("EDITOR")
 	if editor == "" {
-		return fmt.Errorf("EDITOR env variable not set")
+		return editorNotSet
 	}
 
 	cmd := exec.Command(editor, cmds...)
