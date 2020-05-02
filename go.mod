358077f079caf838713a84a39325b7d42c3647da
jbpratt78 <jbpratt78@gmail.com>
2020-05-02 11:02:07 -0500

    update deps and fix module definition, remove config tests

--- go.mod
+++ go.mod
@@ -1,11 +1,13 @@
-module jrnl
+module github.com/jbpratt78/jrnl
 
 go 1.14
 
 require (
 	github.com/99designs/keyring v1.1.5
+	github.com/keybase/go-keychain v0.0.0-20200502122510-cda31fe0c86d // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/shurcooL/go v0.0.0-20191216061654-b114cc39af9f // indirect
 	github.com/shurcooL/markdownfmt v0.0.0-20191117054414-21fe95c248e9
-	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79
+	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path"
-	"reflect"
 	"testing"
 	"time"
 )
@@ -56,58 +53,8 @@ func TestEditorNotSet(t *testing.T) {
 
 	if err := edit(
 		file.Name(),
-		"-c", ":$put _",
-		"-c", "$ s/^/### testing/",
 		"-c", ":wq",
 	); err != editorNotSet {
 		t.Fatalf("excepted to fail when editor not set. got=%s", err.Error())
-	}
-}
-
-func TestLoadConfigNotExists(t *testing.T) {
-	dir, err := ioutil.TempDir("", ".config")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dir)
-
-	cfg, err := loadConfig(dir)
-	if err != nil {
-		t.Fatalf("loadConfig(%q) failed with %s", dir, err.Error())
-	}
-
-	if cfg.Path != "" {
-		t.Fatalf("excepted new, empty configuration. got=%v", cfg)
-	}
-}
-
-func TestLoadConfigExists(t *testing.T) {
-	dir, err := ioutil.TempDir("", ".config")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.RemoveAll(dir)
-	// make config and save it, then load it
-	want := &config{Path: "testing"}
-	if err = os.Mkdir(dir+configDir, os.ModePerm); err != nil {
-		t.Fatal(err)
-	}
-
-	data, err := json.Marshal(want)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err = ioutil.WriteFile(path.Join(dir, "jrnl", "config.json"), data, os.ModePerm); err != nil {
-		t.Fatal(err)
-	}
-
-	got, err := loadConfig(dir)
-	if err != nil {
-		t.Fatalf("loadConfig(%q) failed with %s", dir, err.Error())
-	}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("loadConfig failed. got=%v; want=%v", got, want)
 	}
 }
